35bb128759aa7cce03dee6b5d8fab4e3ecbe5cf9
Justin Sitter <justin@jaytria.com>
2020-09-19 22:38:25 -0700

    Use let instead of var

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -1,12 +1,7 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {stickynotsticky()};
-
-// Get the navbar
-var navbar = document.getElementById("primary-menu-region");
-var adminnav = document.getElementById("toolbar-administration");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+let navbar = document.getElementById("primary-menu-region");
+let adminnav = document.getElementById("toolbar-administration");
+let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickynotsticky() {
